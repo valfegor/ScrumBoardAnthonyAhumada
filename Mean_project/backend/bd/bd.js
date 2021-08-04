91c9c24a7a0bb9480c71f524414289f245345a2c
Anthony Ahumada <locopala99@gmail.com>
2021-08-04 15:25:39 +0200

    save point

---
+++ Mean_project/backend/bd/bd.js
@@ -0,0 +1,5 @@
+//Aqui me toco ir a revisar por que no me acordaba
+//debo tener presente que voy a utilizar la libreria de moongose para la conexion con mongo.
+
+const mongoose = require("mongoose");
+
