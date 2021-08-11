d6198f37c204d0307618f601205ccbd64495d5a5
Anthony Ahumada <locopala99@gmail.com>
2021-08-11 13:53:02 +0200

    more js

---
+++ Mean_project/backend/controller/usercontroller.js
@@ -0,0 +1,14 @@
+//el controlador necesita el modelo (siempre)(no olvidar)
+const User = require("../models/user");
+const Role = require("../models/role");
+const hash = require("bcrypt");
+
+
+const registerUser = async (req,res) => {
+
+}
+
+
+const listUser = async (req,res) => {
+    
+}
