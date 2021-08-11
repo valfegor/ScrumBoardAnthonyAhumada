ce668a4c9c208c179f5b8ba95783d36350571f99
Anthony Ahumada <locopala99@gmail.com>
2021-08-11 14:46:25 +0200

    more js

--- Mean_project/backend/index.js
+++ Mean_project/backend/index.js
@@ -12,6 +12,9 @@ const {dbConnection} = require('./bd/bd');
 //aqui llamo a la ruta con la variable ROle
 const Role = require("./routes/rolroutes");
 
+//aqui llamo la ruta de los usuarios.
+
+const User = require("./routes/userroutes");
 //requiere la libreria que instalamos dotenv para configurar todas las variables de entorno
 //si no esta esta linea genera fallos a la hora de generar los archivos.
 
@@ -27,6 +30,8 @@ app.use(express.json());
 app.use(cors());
 //esta es la ruta que va a utilizar nuestra aplicacion
 app.use("/api/role",Role);
+//aqui colocamos nuestra ruta.
+app.use("/api/user",User);
 app.listen(process.env.PORT,()=>console.log("Backend Server Running  Ok On Port",process.env.PORT))
 
 //Esta parte se me olvido y me toco revisar como un pendejo -.-
