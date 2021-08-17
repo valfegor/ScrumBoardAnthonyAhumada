d81a286c8d0681afdf129f40714bdc7018fc319c
Anthony Ahumada <locopala99@gmail.com>
2021-08-17 10:31:36 +0200

    more js

--- Mean_project/backend/controller/auth.js
+++ Mean_project/backend/controller/auth.js
@@ -14,4 +14,14 @@ const User = require('../models/user');
 const login = async (req,res) => {
     //aqui le envio el correo electronico
     let user = await User.findOne({email:req.body.email});
+
+    if(!user) return res.status(400).send("Sorry check the email or the password.");
+
+    //ahora validamos la contraseña.
+
+    //aqui utilizando el bcrypt comparo si lo que estoy enviando es lo mismo que se encuentra en la coleccion.
+    const hash = bcrypt.compare(req.body.password, user.password);
+
+
+    if(!hash) return res.status(400).send("Sorry check the password or the email.");
 }
